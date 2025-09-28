fb50e851e9e456414a3409a5e3e43af6b7ed1185
RyanTN8 <ryantn8@gmail.com>
2025-09-28 14:31:27 -0700

    new page

---
+++ my-app/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    < main className =" min -h - screen flex items - center justify - center bg - gray-100" >
+      <h1 className =" text -3 xl font - bold text - purple -600" >
+        Ryan is very excited to learn in WDB bootcamp!
+      </h1 >
+    </ main >
+  );
+}
