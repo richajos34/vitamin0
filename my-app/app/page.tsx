76cf210feaf5d16cec8c61c5420ab096c7e2c301
RyanTN8 <ryantn8@gmail.com>
2025-09-29 22:05:10 -0700

    Changed background and text colors

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    < main className =" min -h - screen flex items - center justify - center bg - gray-100" >
-      <h1 className =" text -3 xl font - bold text - purple -600" >
-        Ryan is very excited to learn in WDB bootcamp!
+    < main className =" min -h - screen flex items - center justify - center bg - white-100" >
+      <h1 className =" text -3 xl font - bold text - black -600" >
+        Excited to learn in WDB bootcamp!
       </h1 >
     </ main >
   );
