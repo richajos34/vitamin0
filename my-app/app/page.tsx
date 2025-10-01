a337c9d93161581c2f5021b16bcc93364da3d5d2
sophie nguyen <sophie.nguyen@berkeley.edu>
2025-09-30 23:00:28 -0700

    changed color + text

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -3,8 +3,8 @@
 export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-3xl font-bold text-purple-600" >
-        This is for Vitamin 0!
+      <h1 className="text-3xl font-bold text-sky-600" >
+        hi this is sophie yayay
       </h1>
     </main>
   );
