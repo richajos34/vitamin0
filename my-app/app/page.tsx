ed0563fb75f72739a99ab269aaace5d4b80ff354
Kelly Ha <kellyha@berkeley.edu>
2025-09-30 22:50:09 -0700

    i added new text

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-gray-100">
       <h1 className="text-3xl font-bold text-purple-600" >
-        This is for Vitamin 0!
+        This is for Vitamin 0! Hi Kelly
       </h1>
     </main>
   );
