e74795067b4bc3bd04247792d94e73f7c4ca1bc0
sophie nguyen <sophie.nguyen@berkeley.edu>
2025-10-08 18:52:56 -0700

    used useState hook

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,10 +1,20 @@
 // pages / index . tsx
+"use client"; 
+
+import { useState  } from "react";
 
 export default function Home() {
+  const [text, setText] = useState('yayay');
+
+  const changeText = () => {
+    setText(text === 'yayay' ? 'boooo' : 'yayay');
+  };
+
   return (
     <main className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-3xl font-bold text-sky-600" >
-        hi this is sophie yayay
+      <h1 className="text-3xl font-bold text-sky-600"  onClick={changeText}>
+        hi this is sophie{" "}
+        {text}
       </h1>
     </main>
   );
